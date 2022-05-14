9960c11a6ce0666fea16b0ff7c25aef5a3820d05
Anngre <gren.anna.joanna@gmail.com>
2022-05-14 11:48:10 +0200

    add Income component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,8 @@
 import styles from './Button.module.css'
 
 
-export default function Button({label, onClick}) {
+export default function Button({label, onClick, color='green'}) {
   return (
-    <button className={styles.btn} onClick={onClick}>{label}</button>
+    <button className={` ${styles.btn} ${color === 'red' ? styles.btnRed : styles.btnGreen}`} onClick={onClick}>{label}</button>
   )
 }
--- src/components/income/Income.js
+++ src/components/income/Income.js
@@ -1,8 +1,60 @@
+import React, { useMemo, useState } from 'react'
+import Button from '../button/Button'
 import styles from './Income.module.css'
 
 
-export default function Income() {
+export default function Income({currentBudget}) {
+  const [incomes, setIncomes] = useState(currentBudget.incomes)
+
+  const incomesSum = useMemo(() => {
+    return incomes.reduce((acc, income) => {
+      return acc + income.amount
+   }, 0)
+  },[incomes])
+
+
+  const handleAddClick = () => {
+    setIncomes([...incomes, {type: '', amount: 0}])
+  }
+
+  const handleDelClick = () => {
+    if (incomes.length > 1) {
+      setIncomes(incomes.slice(0,-1))
+    }
+  }
+
+  const handleChange = (key, value, index) => {
+    setIncomes(incomes.map((income, i) => { 
+      if(i !== index) {
+        return income
+      }  
+      return (key === 'type' ? {...income, type: value || ''} : {...income, amount: parseFloat(value) || 0}) 
+    }))
+  }
+  
+  
   return (
-    <div>Income</div>
+    <div className={styles.incomesContainer}>
+      <h3 className={styles.budgetTitle}>{currentBudget.budgetTitle}</h3>
+      <div className={styles.addIncomeBox}>
+        <h4 className={styles.title}>Incomes:</h4>
+        <Button label='+' onClick={handleAddClick}/>
+        <Button label='-' onClick={handleDelClick} color='red'/>
+      </div>
+      <div className={styles.incomes}>
+        <span className={styles.inputType}>Type of the income:</span>
+        <span className={styles.inputType}>Amount:</span>
+        {incomes.map((income, i) => {
+          return (<React.Fragment key={i}>
+            <input className={styles.incomeInput} type='text' value={income.type || ''} onChange={(e) => handleChange('type', e.target.value, i)}></input>
+            <input className={styles.incomeInput} type='number' value={income.amount || ''} onChange={(e)=>handleChange('amount', e.target.value, i)}></input>
+          </React.Fragment>)
+        })}
+        
+        <div className={styles.incomeTotal}>Total income:</div>
+        <div className={styles.incomeTotalAmount}>{incomesSum}</div>
+      </div>
+
+    </div>
   )
 }
